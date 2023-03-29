c7791c617b4e6b1f11a430baa830399b5b986dfc
Camille Arce <thecamillearce@gmail.com>
2023-03-29 17:41:00 +0000

    initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,47 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+  def homepage
+    render({ :template => "rps_templates/homepage.html.erb", :layout => "layouts/wrapper.html.erb" })
+  end
 
+  def play_rock
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "rock"
+      @outcome = "tied"
+    elsif @computer_move == "paper"
+      @outcome = "lost"
+    elsif @computer_move == "scissors"
+      @outcome = "won"
+    end
+    render({ :template => "rps_templates/user_rock.html.erb", :layout => "layouts/wrapper.html.erb" })
+  end
+
+  def play_paper
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "rock"
+      @outcome = "won"
+    elsif @computer_move == "paper"
+      @outcome = "tie"
+    elsif @computer_move == "scissors"
+      @outcome = "lost"
+    end
+    render({ :template => "rps_templates/user_paper.html.erb", :layout => "layouts/wrapper.html.erb" })
+  end
+
+  def play_scissors
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "rock"
+      @outcome = "lost"
+    elsif @computer_move == "paper"
+      @outcome = "won"
+    elsif @computer_move == "scissors"
+      @outcome = "tied"
+    end
+
+    render({ :template => "rps_templates/user_scissors.html.erb", :layout => "layouts/wrapper.html.erb" })
+  end
 end
